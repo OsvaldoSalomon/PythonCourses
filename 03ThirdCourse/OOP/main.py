0a1177371308b5f464ea3a1d24d493ad8668ed03
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2021-02-04 12:49:36 -0500

    Modified main

--- 03ThirdCourse/OOP/main.py
+++ 03ThirdCourse/OOP/main.py
@@ -1,21 +1,39 @@
-class Square:
-    def __init__(self, side_lenght):
-        self.side_lenght = side_lenght
+class Email:
+    valid_domain = "epicprogram.io"
 
-    def compute_area(self):
-        return pow(self.side_lenght, 2)
+    def __init__(self, subject, body, from_recipient):
+        self.subject = subject
+        self.body = body
+
+        split = from_recipient.split("@")
+        if split[1] != self.valid_domain:
+            raise Exception("Invalid domain")
+
+        self.__from_recipient = from_recipient
+
+    @property
+    def from_recipient(self):
+        return self.__from_recipient
+
+    @from_recipient.setter
+    def from_recipient(self, from_recipient):
+        split = from_recipient.split("@")
+        if split[1] != self.valid_domain:
+            raise Exception("Invalid domain")
+        self.__from_recipient = from_recipient
+
+    def send(self, to):
+        print(f"Sending email from: {self.__from_recipient} with subject: {self.subject} and body: {self.body} to: {to}")
 
 
 if __name__ == "__main__":
-    example_square = Square(15)
-    print(example_square.compute_area())
+    greeting_email = Email("Welcome!", "Welcome to this program!", "osvaldo@epicprogram.io")
+    bf_email = Email("Black Friday 2020!", "50% discount on our platform", "bf@epicprogram.io")
 
-    small_square = Square(2)
-    print(small_square.compute_area())
+    Email.valid_domain = "example.com"
 
-    big_square = Square(1000)
-    print(big_square.compute_area())
+    greeting_email.from_recipient = "greeting@example.com"
+    bf_email.from_recipient = "bf@example.com"
 
-
-
-
+    greeting_email.send("bob@nasa.gov")
+    bf_email.send("alex@gmail.com")
