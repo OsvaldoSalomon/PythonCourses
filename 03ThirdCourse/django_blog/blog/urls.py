796e4fa75acc3ba21272b3d4b8c9f79050d493d3
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2021-02-20 09:56:42 -0500

    The templates and the models have been created, as well as the database

--- 03ThirdCourse/django_blog/blog/urls.py
+++ 03ThirdCourse/django_blog/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import Home
+from .views import Home, Article
 
 urlpatterns = [
-    path('', Home.as_view())
+    path('', Home.as_view()),
+    path('articles', Article.as_view())
 ]
--- 03ThirdCourse/django_blog/blog/views.py
+++ 03ThirdCourse/django_blog/blog/views.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-
+from blog.models import ArticleModel
 
 class Home(View):
     def get(self, request):
@@ -9,3 +11,26 @@ class Home(View):
 
     def post(self, request):
         return HttpResponse("[POST] Welcome to my blog!")
+
+
+class Article(View):
+    def get(self, request):
+
+        articles = ArticleModel.objects.all()
+        # articles = [
+        #     {
+        #         "title": "Python 5 is officially announced",
+        #         "category": "tech",
+        #         "author": "Guido van Rossum",
+        #         "content": "Just joking, we will have Python 3 for some years",
+        #         "creation_date": datetime.now()
+        #     },
+        #     {
+        #         "title": "Tesla goes bankrupt",
+        #         "category": "auto",
+        #         "author": "Elon Musk",
+        #         "content": "Just trying to increase Tesla share price here!",
+        #         "creation_date": datetime.now()
+        #     }
+        # ]
+        return render(request, "articles.html", {"articles": articles})
