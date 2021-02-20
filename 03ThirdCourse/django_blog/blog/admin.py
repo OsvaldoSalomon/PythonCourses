e99e6d06c2f49ff47afe223a2d09d6f3229c5e96
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2021-02-20 13:17:00 -0500

    Created the Tests

--- 03ThirdCourse/django_blog/blog/admin.py
+++ 03ThirdCourse/django_blog/blog/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from blog.models import ArticleModel
 
+
 # Register your models here.
-admin.site.register(ArticleModel)
+@admin.register(ArticleModel)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "creation_date")
--- 03ThirdCourse/django_blog/blog/tests.py
+++ 03ThirdCourse/django_blog/blog/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class ArticleTest(TestCase):
+    def test_article_created_success(self):
+        pass
