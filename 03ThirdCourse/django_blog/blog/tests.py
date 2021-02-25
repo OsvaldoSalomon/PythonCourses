9adf96dc1577514e3330c70ff0a635c4fdfd6682
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2021-02-25 05:47:17 -0500

    Finished the testing and done with this project

--- 03ThirdCourse/django_blog/blog/tests.py
+++ 03ThirdCourse/django_blog/blog/tests.py
@@ -1,7 +1,17 @@
 from django.test import TestCase
-
+from blog.models import ArticleModel
+from datetime import datetime
+from django.utils import timezone
 
 # Create your tests here.
 class ArticleTest(TestCase):
     def test_article_created_success(self):
-        pass
+        ArticleModel.objects.create(title="test article", category="test category", author="test author", content="test content", creation_date=datetime.now(tz=timezone.utc))
+        article = ArticleModel.objects.get(title="test article")
+        self.assertEqual(article.category, "test category")
+
+
+class BlogPagesTest(TestCase):
+    def test_home_page_content(self):
+        res = self.client.get("/blog/")
+        self.assertEqual(res.content, b"Welcome to my blog!")
