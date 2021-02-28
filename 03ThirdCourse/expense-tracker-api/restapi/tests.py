9964e8a64a5fae1edb9647f908a2031e3d6858de
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2021-02-28 12:17:23 -0500

    Added model and tests for expenses.

--- 03ThirdCourse/expense-tracker-api/restapi/tests.py
+++ 03ThirdCourse/expense-tracker-api/restapi/tests.py
@@ -1,12 +1,21 @@
-# from django.test import TestCase
-from unittest import TestCase
+from django.test import TestCase
+import models
+
+# "E:/osvaldo/PythonExercises/03ThirdCourse/expense-tracker-api/restapi"
 
 
 # Create your tests here.
-def two_integers_sum(a, b):
-    return 0
+class TestModels(TestCase):
+    def test_expense(self):
+        expense = models.Expense.objects.create(
+            amount=249.99,
+            merchant="amazon",
+            description="anc headphones",
+            category="music",
+        )
+        inserted_expense = models.Expense.objects.get(ok=expense.id)
 
-
-class TestSum(TestCase):
-    def test_sum(self):
-        self.assertEqual(two_integers_sum(1, 2), 3)
+        self.assertEqual(249.99, inserted_expense.amount)
+        self.assertEqual("amazon", inserted_expense.merchant)
+        self.assertEqual("anc headphones", inserted_expense.description)
+        self.assertEqual("music", inserted_expense.category)
